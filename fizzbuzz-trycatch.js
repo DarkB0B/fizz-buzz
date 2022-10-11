b933e55a9b0a13c9c244b1792a05034564111190
DarkB0B <miecze123@onet.pl>
2022-10-11 10:56:16 +0200

    try/catch still doesnt work, idk works idk why

--- fizzbuzz-trycatch.js
+++ fizzbuzz-trycatch.js
@@ -1,32 +1,44 @@
-//doesn't work idk why
-
-
-for(var i = 0; i < 100; i++){
-    try{(i % 3 && i % 5) || throwexception}    
+//doesn't work
+function tryy(x, y)
+{
+    try
+    {x || throwexception}
     catch(e)
     {
-        console.log(i);
-        console.log(" ");    
-        i++;   
+        y;
     }
-    try{!(i % 3 && i % 5) || throwexception}    
-    catch(e)
-    {       
-        console.log("fizz buzz");
-        console.log(" ");
-        i++;        
-    }
-    try{(i % 3) || throwexception}    
-    catch(e)
-    {
-        console.log("fizz");
-        console.log(" ");        
-    }
-    try{(i % 5) || throwexception}    
-    catch(e)
-    {
-        console.log("buzz");
-        console.log(" ");       
-    }
-   
 }
+for(var i = 0; i < 100; i++){
+    tryy(!(i % 3 && i % 5), console.log("fizz buzz"));
+    tryy((!i % 3), console.log("fizz"));
+    tryy(!(i % 5), console.log("buzz"));
+    tryy((i % 3 && i % 5), console.log(i));
+}
+// for(var i = 0; i < 100; i++){
+//     try{!(i % 3 && i % 5) || throwexception}    
+//     catch(e)
+//     {       
+//         console.log("fizz buzz");
+//         console.log(" ");
+//         i++;        
+//     }
+//     try{!(i % 3) || throwexception}    
+//     catch(e)
+//     {
+//         console.log("fizz");
+//         console.log(" ");        
+//     }
+//     try{!(i % 5) || throwexception}    
+//     catch(e)
+//     {
+//         console.log("buzz");
+//         console.log(" ");       
+//     }
+//     try{(i % 3 && i % 5) || throwexception}    
+//     catch(e)
+//     {
+//         console.log(i);
+//         console.log(" ");    
+          
+//     }
+    //}
